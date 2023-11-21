9d5d0b78b286dea4d2fd6113809ee6470b6f3935
francoismavunila <mavunila002@gmail.com>
2023-11-21 10:47:53 +0000

    third commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT ||3000;
 const cors = require('cors')
 const axios = require('axios');
-require('dotenv').config();
+//require('dotenv').config();
 
 // Define routes
 
